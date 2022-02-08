2192b2b72c0fb77bc5abeca69305eb35464167f4
Ongsanaja <User@DESKTOP-9DJL3BV>
2022-02-08 16:03:43 +0700

    SAVE update1

---
+++ src/main/java/net/mcreator/dinoanimatronic/item/NailItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.dinoanimatronic.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.dinoanimatronic.DinoanimatronicModElements;
+
+@DinoanimatronicModElements.ModElement.Tag
+public class NailItem extends DinoanimatronicModElements.ModElement {
+	@ObjectHolder("dinoanimatronic:nail")
+	public static final Item block = null;
+
+	public NailItem(DinoanimatronicModElements instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return -1f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("nail"));
+	}
+}
